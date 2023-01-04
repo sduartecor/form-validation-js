ab396f3cf54d38c30116d361ca4e9288000823cf
sduartecor <santiagoduartecor@outlook.com>
2023-01-04 15:13:13 +0000

    Updates

--- src/app.js
+++ src/app.js
@@ -30,34 +30,56 @@ window.onload = function() {
     if (document.getElementById("cardnumber").value == "") {
       document.getElementById("invalidCredit").innerHTML =
         "Please provide a valid Credit Card.";
+    } else {
+      document.getElementById("invalidCredit").style.display = "none";
     }
     if (document.getElementById("cvc").value == "") {
       document.getElementById("invalidCvc").innerHTML =
         "Please provide a valid CVC.";
+    } else {
+      document.getElementById("invalidCvc").style.display = "none";
     }
     if (document.getElementById("amount").value == "") {
       document.getElementById("invalidAmount").innerHTML =
         "Please provide a valid amount.";
+    } else {
+      document.getElementById("invalidAmount").style.display = "none";
     }
-    if (document.getElementById("firstname").value == "") {
+    if (
+      document.getElementById("firstname").value == "" ||
+      !/^[a-zA-Z]*$/g.test(document.getElementById("firstname").value)
+    ) {
       document.getElementById("invalidFName").innerHTML =
         "Please provide a valid first Name.";
+    } else {
+      document.getElementById("invalidFName").style.display = "none";
     }
-    if (document.getElementById("lastname").value == "") {
+    if (
+      document.getElementById("lastname").value == "" ||
+      !/^[a-zA-Z]*$/g.test(document.getElementById("lastname").value)
+    ) {
       document.getElementById("invalidLName").innerHTML =
         "Please provide a valid last Name.";
+    } else {
+      document.getElementById("invalidLName").style.display = "none";
     }
     if (document.getElementById("city").value == "") {
       document.getElementById("invalidCity").innerHTML =
         "Please provide a valid city.";
+    } else {
+      document.getElementById("invalidCity").style.display = "none";
     }
     if (document.getElementById("state").value == "") {
       document.getElementById("invalidState").innerHTML =
         "Please provide a valid state.";
+    } else {
+      document.getElementById("invalidState").style.display = "none";
     }
     if (document.getElementById("zip").value == "") {
       document.getElementById("invalidZip").innerHTML =
         "Please provide a valid zip.";
+    } else {
+      document.getElementById("invalidZip").style.display = "none";
     }
     if (
       !(
@@ -69,6 +91,11 @@ window.onload = function() {
     ) {
       document.getElementById("invalidCard").innerHTML =
         "Please provide a valid cards.";
+    } else {
+      document.getElementById("invalidCard").style.display = "none";
     }
+
+    var z = document.getElementById("firstname").value.match(/^\d+/);
+    console.log();
   }
 };
